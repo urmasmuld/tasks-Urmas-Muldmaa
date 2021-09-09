d95109d7306a495c403faff637a11365ea43e1a6
Urmas Muldmaa <urmas@vau.local>
2021-09-09 10:06:04 +0300

    first commit

---
+++ task6.js
@@ -0,0 +1,18 @@
+// Function to display inserted string "n" times
+// https://github.com/urmasmuld
+// 09.09.2021
+
+const rep = "repeat " // assign String
+function repeatStr(input, times) { // function to repeat given string 'n' times
+    let result = input.repeat(times);  // Repeat 'n' times
+    if (times === undefined) { // Check if variable 'times' is present
+        console.log(times);
+    } else {
+        console.log(result);
+    }
+}
+ 
+repeatStr(rep);
+repeatStr(rep,2);
+repeatStr(rep,3);
+repeatStr(rep,12);
