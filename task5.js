bc2a7b52f145f5fb81e4cdf804d6b80e3be83aa1
Urmas Muldmaa <urmas@vau.local>
2021-09-09 09:46:22 +0300

    second commit

--- task5.js
+++ task5.js
@@ -1,3 +1,7 @@
+const firstval =  ""
+const secondval =  "tere"
+const thirdval =  "minu nimi on John"
+
 function testString(input){ // function to check wheter it has some value or not
 let answer = ""
     if (input) {
@@ -8,6 +12,14 @@ let answer = ""
     return answer
 }
 
-console.log(testString(''))
-console.log(testString('tere'))
-console.log(testString('minu nimi on John'))
+// console.log(testString(firstval))
+// console.log(testString(secondval))
+// console.log(testString(thirdval))
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  
+  console.log(capitalizeFirstLetter(firstval));
+  console.log(capitalizeFirstLetter(secondval));
+  console.log(capitalizeFirstLetter(thirdval));
