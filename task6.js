0f7324d164877dafba30f936e88685b52c2fff6c
Urmas Muldmaa <urmas@vau.local>
2021-09-09 10:09:11 +0300

    second commit

--- task6.js
+++ task6.js
@@ -6,12 +6,12 @@ const rep = "repeat " // assign String
 function repeatStr(input, times) { // function to repeat given string 'n' times
     let result = input.repeat(times);  // Repeat 'n' times
     if (times === undefined) { // Check if variable 'times' is present
-        console.log(times);
+        console.log("Hi!");
     } else {
         console.log(result);
     }
 }
- 
+
 repeatStr(rep);
 repeatStr(rep,2);
 repeatStr(rep,3);
