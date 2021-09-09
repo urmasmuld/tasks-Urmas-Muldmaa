46bc52051ac0e508b25a46ebe7b310e157eb8c22
Urmas Muldmaa <urmas@vau.local>
2021-09-09 09:26:45 +0300

    second commit

--- task4.js
+++ task4.js
@@ -1,11 +1,17 @@
-const arrColor = ["Red", "Green", "White", "Black",""]
-// const Colors = arrColor.toString()
+const arrColor = ["Red", "Green", "White", "Black"] // set array
+let arrayLength = arrColor.length                   // check array length
 
-const colorsArr = arrColor.join(",")
-const Colors = colorsArr.replace(/,$/,".")
-const Colors1 = colorsArr
-const Colors2 = arrColor.join("+")
-
-console.log(Colors)
-console.log(Colors1)
-console.log(Colors2)
+function colorsX(vars){ 
+    let colors =  ""    // set variable
+    for (var i = 0; i < arrayLength; i++) { // start for loop to display values
+    if (i+1 == arrayLength) { // if is last value, then dont add delimiter
+        colors += arrColor[i]
+    } else {
+        colors += arrColor[i] + vars // if not last value, then add delimiter
+    }
+}
+    return colors       // return value
+}
+console.log(colorsX(",") + ".")
+console.log(colorsX(",") + ",")
+console.log(colorsX("+") + "+")
