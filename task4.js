2ecc32ca6896df38c7d89f1713d77b24f3f08577
Urmas Muldmaa <urmas@vau.local>
2021-09-16 14:50:41 +0300

    New 4.0 task

--- task4.js
+++ task4.js
@@ -1,17 +1,11 @@
+// New version of task 4.0
 const arrColor = ["Red", "Green", "White", "Black"] // set array
-let arrayLength = arrColor.length                   // check array length
+let color = "" // set color variable
 
-function colorsX(vars){ 
-    let colors =  ""    // set variable
-    for (var i = 0; i < arrayLength; i++) { // start for loop to display values
-    if (i+1 == arrayLength) { // if is last value, then dont add delimiter
-        colors += arrColor[i]
-    } else {
-        colors += arrColor[i] + vars // if not last value, then add delimiter
-    }
+for (let i = 0; i < arrColor.length; i++) { // loop through array
+    color = color + arrColor[i] + ',' // add comma to the end of each value
 }
-    return colors       // return value
-}
-console.log(colorsX(","))
-console.log(colorsX(", "))
-console.log(colorsX(" + "))
+
+console.log(color.replace(/.$/,"."))
+console.log(color)
+console.log(color.split(",").join("+"))
