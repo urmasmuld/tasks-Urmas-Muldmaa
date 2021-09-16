701dbb201d4a37d70f9be3fabf7250d13bcbd57a
Urmas Muldmaa <urmas@vau.local>
2021-09-16 14:07:23 +0300

    changed birthday constant from String to Date 2

--- task2.js
+++ task2.js
@@ -2,7 +2,20 @@ const firstName = "Urmas"
 // const birthday = "26.02.1981"
 const birthday = new Date(1981, 02, 26)
 const Hobbies = ['Programmeerimine','Tugitoolisport']
-const formattedBirthDay = birthday.getDate() + '.' + birthday.getMonth() + '.' + birthday.getFullYear()
+let birthdayDay
+let birthdayMonth
+if (birthday.getDate() < 10) {
+    birthdayDay = '0' + birthday.getDate()
+} else {
+    birthdayDay = birthday.getDate()
+}
+if (birthday.getMonth() < 10) {
+    birthdayMonth = '0' + birthday.getMonth()
+} else {
+    birthdayMonth = birthday.getMonth()
+}
+
+const formattedBirthDay = birthdayDay + '.' + birthdayMonth + '.' + birthday.getFullYear()
 //const aboutMe = {firstName: firstName, BirthDate : Day + "." + Month + "." + Year , Hobbies: Hobbies}
 const aboutMe = {
     firstName,
