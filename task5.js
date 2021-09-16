dcb19bcc620de5fba9e15e4281cc45b5a0db2056
Urmas Muldmaa <urmas@vau.local>
2021-09-16 15:05:44 +0300

    Redesigned task5.js

--- task5.js
+++ task5.js
@@ -2,19 +2,9 @@ const firstval =  ""
 const secondval =  "tere"
 const thirdval =  "minu nimi on John"
 
-function testString(input){ // function to check wheter it has some value or not
-let answer = ""
-    if (input) {
-        answer =  "true"
-    } else {
-        answer = "false"
-    }
-    return answer
-}
-
-// console.log(testString(firstval))
-// console.log(testString(secondval))
-// console.log(testString(thirdval))
+console.log(Boolean(firstval !== ""))
+console.log(Boolean(secondval !== ""))
+console.log(Boolean(thirdval !== ""))
 
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
